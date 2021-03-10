da2374d98ab738baab34a7d97da22f4e9fe533f3
intanvionasari <vionasari838@gmail.com>
2021-03-10 15:24:15 +0700

    menambah dropdown, result, serta raised button

--- lib/main.dart
+++ lib/main.dart
@@ -14,12 +14,8 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   double _inputPanjang = 0; //membuat variable
   double _inputLebar = 0; //membuat variable
-  double _tigapuluh = 0; //membuat variable
-  double _empatpuluh = 0; //membuat variable
-  double _limapuluh = 0; //membuat variable
-  final inputpjController = TextEditingController(); //membuat controller  
+  final inputpjController = TextEditingController(); //membuat controller
   final inputlbController = TextEditingController(); //membuat controller
-
 
   var listItem = ["30X30", "40X40", "50X50"]; //variable pada list
   List<String> listViewItem = List<String>(); //untuk list item
@@ -29,7 +25,7 @@ class _MyAppState extends State<MyApp> {
   void dropDownOnChanged(String changeValue) {
     //fungsi untuk dropdown
     setState(() {
-      _newValue = changeValue;
+      _newValue = changeValue;//fungsi untuk mengubah value pada dropdown
     });
   }
 
@@ -61,44 +57,114 @@ class _MyAppState extends State<MyApp> {
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: Scaffold(
+        resizeToAvoidBottomInset: false,//digunakan agar tidak overflowed
         appBar: AppBar(
           title: Text("HomeSweetHome"), //title tampilan kanan atas aplikasi
         ),
-        body: Container(//container
-          padding: EdgeInsets.only( bottom:20.0),//mengatur jarak
-          margin: EdgeInsets.all(15),//mengatur jarak
+        body: Container(
+          //container
+          margin: EdgeInsets.all(5), //mengatur jarak
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,//mengatur jarak
+            mainAxisAlignment: MainAxisAlignment.start, //mengatur jarak
             children: [
-              Image.network('https://4.bp.blogspot.com/-siKbotN6Ids/U3AyMMN60OI/AAAAAAAAAQ8/Coeenl_4BSw/s1600/rumah-bagus-murah.png'),//gambar dari internet
-              Container(//container
-                padding: EdgeInsets.all(11.0),//mengatur jarak
-                child: TextFormField(//membuat text form(kolom yang bisa diisi)
-                  controller: inputpjController,//memanggil controller
-                  inputFormatters: [FilteringTextInputFormatter.digitsOnly],//validasi digit
-                  keyboardType: TextInputType.number,//tipe keyboard
-                  decoration: new InputDecoration(//dekorasi textformfield
-                      hintText: "Masukkan Panjang Ruangan",//text pada textformfield
-                      icon: Icon(Icons.home_outlined),//icon pada textformfield
-                      border: OutlineInputBorder(//border pada textformfield
-                      borderRadius: new BorderRadius.circular(5.0))),//border pada textformfield
+              Image.network(//mengambil gambar secara online
+                  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSd1eki1ZC4-4f7wgcG480nrY9Vc1SYuJx-3g&usqp=CAU',
+                  width: 200.0,
+                  height: 150.0),
+              Container(
+                //container
+                padding: EdgeInsets.all(5.0), //mengatur jarak
+                child: TextFormField(
+                  //membuat text form(kolom yang bisa diisi)
+                  controller: inputpjController, //memanggil controller
+                  inputFormatters: [
+                    FilteringTextInputFormatter.digitsOnly
+                  ], //validasi hanya digit
+                  keyboardType: TextInputType.number, //tipe keyboard
+                  decoration: new InputDecoration(
+                      //dekorasi textformfield
+                      hintText:
+                          "Masukkan Panjang Ruangan(meter)", //text pada textformfield
+                      icon: Icon(Icons.home_outlined), //icon pada textformfield
+                      border: OutlineInputBorder(
+                          //border pada textformfield
+                          borderRadius: new BorderRadius.circular(
+                              2.0))), //border pada textformfield
                 ),
               ),
-              
-              Container(//container
-                padding: EdgeInsets.all(11.0),//mengatur jarak
-                child: TextFormField(//membuat text form(kolom yang bisa diisi)
-                  controller: inputlbController,//memanggil controller
-                  inputFormatters: [FilteringTextInputFormatter.digitsOnly],//validasi digit
-                  keyboardType: TextInputType.number,//tipe keyboard
-                  decoration: new InputDecoration(//dekorasi textformfield
-                      hintText: "Masukkan Lebar Ruangan",//text pada textformfield
-                      icon: Icon(Icons.home_outlined),//icon pada textformfield
-                      border: OutlineInputBorder(//border pada textformfield
-                      borderRadius: new BorderRadius.circular(5.0))),//border pada textformfield
+              Container(
+                //container
+                padding: EdgeInsets.all(5.0), //mengatur jarak
+                child: TextFormField(
+                  //membuat text form(kolom yang bisa diisi)
+                  controller: inputlbController, //memanggil controller
+                  inputFormatters: [
+                    FilteringTextInputFormatter.digitsOnly
+                  ], //validasi hanya digit
+                  keyboardType: TextInputType.number, //tipe keyboard
+                  decoration: new InputDecoration(
+                      //dekorasi textformfield
+                      hintText:
+                          "Masukkan Lebar Ruangan(meter)", //text pada textformfield
+                      icon: Icon(Icons.home_outlined), //icon pada textformfield
+                      border: OutlineInputBorder(
+                          //border pada textformfield
+                          borderRadius: new BorderRadius.circular(
+                              2.0))), //border pada textformfield
                 ),
               ),
-              
+              Container(//container
+                padding: EdgeInsets.only(top: 23.0),//padding untuk mengatur jarak hanya atas saja
+                child: Text(//child yang berisi text
+                  'Pilih Ukuran Ubin',//text
+                  textAlign: TextAlign.center,//agar text berada di tengah atau center
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 19.0),//style pada text
+                ),
+              ),
+              Container(//container
+                child: DropdownButton<String>(//child yang berisi berisi dropdown button
+                  items: listItem.map((String value) {//digunakan untuk pilihan list pada dropdown
+                    return DropdownMenuItem<String>(
+                      value: value,//mengatur value
+                      child: Text(value),//text pada dropdown yang berisi value 
+                    );
+                  }).toList(),//untuk listnya
+                  value: _newValue,//memperbarui value atau memilih value
+                  onChanged: dropDownOnChanged,//memanggil fungsi agar dapat mengganti atau memilih list
+                ),
+              ),
+              Container(//container
+                margin: EdgeInsets.only(top: 20, bottom: 20),//mengatur jarak dengan margin atas dan bawah
+                child: Column(//child berisi kolom
+                  mainAxisAlignment: MainAxisAlignment.center,//mainAxis untuk mengatur jarak kebawah
+                  children: [
+                    Text(
+                      "Anda Membutuhkan Ubin Sebanyak",//text
+                      style: TextStyle(//untuk textstyle seperti bold dan ukuran font
+                          fontWeight: FontWeight.bold, fontSize: 15.0),
+                    ),
+                    Text(
+                      _result.toStringAsFixed(1),//menampilkan result yang sudah diproses hitung
+                      style: TextStyle(fontSize: 30),//style text untuk result
+                    ),
+                    Text(
+                      "Biji",//text
+                      style: TextStyle(fontSize: 20.0),//style text
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                width: 150,
+                height: 50,
+                child: RaisedButton(
+                  onPressed: perhitunganRumus,
+                  child: Text("Hitung Ubin"),
+                  splashColor: Colors.white10,
+                  textColor: Colors.black,
+                  color: Colors.blueGrey,
+                ),
+              )
             ],
           ),
         ),
